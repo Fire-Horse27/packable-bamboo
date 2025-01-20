4e2bd533ac3126ddd10f2e1fc76f3d28ab5f4806
Fire-Horse27 <firehorse2727@gmail.com>
2025-01-20 00:00:47 -0500

    Updated to 1.21.4

--- src/main/java/net/fire_horse27/bamboo/BambooDataGenerator.java
+++ src/main/java/net/fire_horse27/bamboo/BambooDataGenerator.java
@@ -11,7 +11,6 @@ public class BambooDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModBlockTagProvider::new);
 		pack.addProvider(ModItemTagProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
-		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
 	}
 }
